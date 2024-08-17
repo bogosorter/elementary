7c74f1293733e9097ca53617d360debd14f7ad6d
M7kra <luiswbarbosa@gmail.com>
2024-08-17 09:36:04 +0100

    Added sidebar tooltips

--- src/renderer/components/Sidebar.tsx
+++ src/renderer/components/Sidebar.tsx
@@ -11,12 +11,12 @@ export default function Sidebar() {
     const showMarkdownActions = (sidebar === 'markdownActions' || sidebar === 'all') && !preview;
 
     const fileButtons = [
-        { icon: <PlusLg size={20} />, action: () => store.getState().newFile() },
-        { icon: <Folder2Open size={20} />, action: () => store.getState().open() },
-        { icon: <Floppy size={20} />, action: () => store.getState().save() },
-        { icon: preview? <EyeSlash size={20} /> : <Eye size={20} />, action: () => store.getState().togglePreview() },
-        { icon: <Gear size={20} />, action: () => store.getState().openCommandPalette() }
-    ].map(({icon, action}, index) =>
+        { icon: <PlusLg size={20} />, action: () => store.getState().newFile(), tooltip: 'New file' },
+        { icon: <Folder2Open size={20} />, action: () => store.getState().open(), tooltip: 'Open file' },
+        { icon: <Floppy size={20} />, action: () => store.getState().save(), tooltip: 'Save file' },
+        { icon: preview? <EyeSlash size={20} /> : <Eye size={20} />, action: () => store.getState().togglePreview(), tooltip: 'Toggle preview' },
+        { icon: <Gear size={20} />, action: () => store.getState().openCommandPalette(), tooltip: 'Command Palette' }
+    ].map(({icon, action, tooltip}, index) =>
         <button
             className='sidebar-button'
             onClick={action}
@@ -24,21 +24,27 @@ export default function Sidebar() {
             key={index}
         >
             {icon}
+            <div
+                className='tooltip'
+                style={{ backgroundColor: theme.surfaceVariant}}
+            >
+                {tooltip}
+            </div>
         </button>
     );
 
     const markdownButtons = [
-        { icon: <TypeBold size={20} />, action: () => store.getState().bold() },
-        { icon: <TypeItalic size={20} />, action: () => store.getState().italic() },
-        { icon: <TypeStrikethrough size={20} />, action: () => store.getState().strikethrough() },
-        { icon: <CodeSlash size={18} />, action: () => store.getState().inlineCode() },
-        { icon: <Link45deg size={20} />, action: () => store.getState().link() },
-        { icon: <Hash size={20} />, action: () => store.getState().heading() },
-        { icon: <Quote size={20} />, action: () => store.getState().quote() },
-        { icon: <ListUl size={20} />, action: () => store.getState().unorderedList() },
-        { icon: <ListOl size={18} />, action: () => store.getState().orderedList() },
-        { icon: <ListCheck size={16} />, action: () => store.getState().todoList() }
-    ].map(({icon, action}, index) =>
+        { icon: <TypeBold size={20} />, action: () => store.getState().bold(), tooltip: 'Bold' },
+        { icon: <TypeItalic size={20} />, action: () => store.getState().italic(), tooltip: 'Italic' },
+        { icon: <TypeStrikethrough size={20} />, action: () => store.getState().strikethrough(), tooltip: 'Strikethrough' },
+        { icon: <CodeSlash size={18} />, action: () => store.getState().inlineCode(), tooltip: 'Inline code' },
+        { icon: <Link45deg size={20} />, action: () => store.getState().link(), tooltip: 'Link' },
+        { icon: <Hash size={20} />, action: () => store.getState().heading(), tooltip: 'Heading' },
+        { icon: <Quote size={20} />, action: () => store.getState().quote(), tooltip: 'Quote' },
+        { icon: <ListUl size={20} />, action: () => store.getState().unorderedList(), tooltip: 'Unordered list' },
+        { icon: <ListOl size={18} />, action: () => store.getState().orderedList(), tooltip: 'Ordered list' },
+        { icon: <ListCheck size={16} />, action: () => store.getState().todoList(), tooltip: 'Todo list' }
+    ].map(({icon, action, tooltip}, index) =>
         <button
             className='sidebar-button'
             onClick={action}
@@ -46,6 +52,12 @@ export default function Sidebar() {
             key={index}
         >
             {icon}
+            <div
+                className='tooltip'
+                style={{ backgroundColor: theme.surfaceVariant}}
+            >
+                {tooltip}
+            </div>
         </button>
     );
 
