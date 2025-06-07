e4faadba08c290195968daae14538ae124cead7f
bogosorter <luiswbarbosa@gmail.com>
2025-06-07 15:59:34 +0100

    remove temporary html file on export

--- src/main/utils/export.ts
+++ src/main/utils/export.ts
@@ -17,26 +17,26 @@ export async function pandocAvailable() {
 }
 
 export async function exportToPDF(input: string, output: string) {
-    const htmlPath = join(dirname(input), `${basename(input, '.md')}.html`);
+    const htmlPath = join(dirname(input), `${basename(input, '.md')}_elementary_temp.html`);
     const cssPath = join(app.getAppPath(), 'assets', 'github.css');
     const templatePath = join(app.getAppPath(), 'assets', 'export.html');
 
     try {
         await execAsync(`pandoc "${input}" -o "${htmlPath}" --template="${templatePath}" --css="${cssPath}" --self-contained`);
-        await htmlFileToPdf(htmlPath, output);
-        //await unlink(htmlPath);
+        await htmlToPdf(htmlPath, output);
+        await unlink(htmlPath);
         return true;
     } catch (e) {
         console.log(e);
         try {
-            //unlink(htmlPath);
+            unlink(htmlPath);
         } catch {} // File may not have been created
 
         return false;
     }
 }
 
-async function htmlFileToPdf(filePath: string, outputPdfPath: string) {
+async function htmlToPdf(filePath: string, outputPdfPath: string) {
     const win = new BrowserWindow({ show: false, webPreferences: { offscreen: true } });
     await win.loadFile(filePath);
     const pdfData = await win.webContents.printToPDF({});
